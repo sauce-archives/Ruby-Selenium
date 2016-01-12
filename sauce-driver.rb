61ba14de986c56dd56b371b3870124b69b2a46b3
DrPhilG <philg@sauce.com>
2016-01-12 11:18:21 -0800

    Copy from DOCS to begin migration

---
+++ sauce-driver.rb
@@ -0,0 +1,21 @@
+require "selenium/webdriver"
+ 
+module SauceDriver
+  class << self
+    def sauce_endpoint
+      "http://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESSKEY@ondemand.saucelabs.com:80/wd/hub"
+    end
+ 
+    def caps
+      caps = {
+        :platform => "Mac OS X 10.9",
+        :browserName => "Chrome",
+        :version => "31"
+      }
+    end
+ 
+    def new_driver
+      Selenium::WebDriver.for :remote, :url => sauce_endpoint, :desired_capabilities => caps
+    end
+  end
+end 
