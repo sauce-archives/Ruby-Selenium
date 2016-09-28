87b41602dab8061fa8db45158b63f79a17a84592
ndmanvar <ndmanvar@yahoo.com>
2016-09-28 10:53:30 -0700

    using https

--- rspec/sauce_driver.rb
+++ rspec/sauce_driver.rb
@@ -1,11 +1,11 @@
 require "selenium/webdriver"
- 
+
 module SauceDriver
   class << self
     def sauce_endpoint
-      "http://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:80/wd/hub"
+      "https://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@ondemand.saucelabs.com:443/wd/hub"
     end
- 
+
     def caps
       caps = {
         :platform => "Mac OS X 10.9",
@@ -13,9 +13,9 @@ module SauceDriver
         :version => "31"
       }
     end
- 
+
     def new_driver
       Selenium::WebDriver.for :remote, :url => sauce_endpoint, :desired_capabilities => caps
     end
   end
-end 
+end
